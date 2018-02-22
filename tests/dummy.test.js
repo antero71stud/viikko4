98b0ba927b402b91c489070fcd7fe96427d2b30a
Antero Oikkonen <antero.oikkonen@gmail.com>
2018-02-22 19:02:00 +0200

    total likes toimii yhden blogin syötteellä

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -6,3 +6,20 @@ test('dummy is called', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+
+test('total likes is called', () => {
+  const blogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const result = listHelper.totalLikes(blogs)
+  expect(result).toBe(5)
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, blog) => {
+        console.log('sum ',sum)
+        console.log('likes ',blog.likes)
+        return sum + blog.likes
+    }
+    return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
