e20bd21fab578e3977d4af6765f68f7c3f32bf66
Antero Oikkonen <antero.oikkonen@gmail.com>
2018-02-22 20:59:59 +0200

    4.5. favorite blog

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -83,3 +83,39 @@ describe('total likes, more blogs', () => {
   })
 
 })
+
+describe('faforate blogs', () => {
+  const twoBlogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const formatBlog = (blog) => {
+    return {
+      title: blog.title,
+      author: blog.author,
+      likes: blog.likes
+    }
+  }
+
+  test('what is favorite of two blogs', () => {
+    const result = listHelper.favoriteBlog(twoBlogs)
+    console.log('result ',result)
+    const expectedBlog = formatBlog(twoBlogs[0])
+    expect(result).toEqual(expectedBlog)
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,14 +4,39 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
     const reducer = (sum, blog) => {
-        console.log('sum ',sum)
-        console.log('likes ',blog.likes)
+        //console.log('sum ',sum)
+        //console.log('likes ',blog.likes)
         return sum + blog.likes
     }
     return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+
+    var maxLikes = ( max,cur) => Math.max(max,cur)
+
+    const formatBlog = (blog) => {
+        console.log('format blog ',blog)
+        console.log('blog.title ',blog[0].title)
+        return {
+          title: blog[0].title,
+          author: blog[0].author,
+          likes: blog[0].likes
+        }
+      }
+
+    var numberOfLikes = blogs.map( blog => blog.likes)
+            .reduce(maxLikes,0)
+
+    const blog1 = blogs.filter(a => a.likes===numberOfLikes)
+    
+    console.log('blog1 ',blog1)
+    
+    return formatBlog(blog1)
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
